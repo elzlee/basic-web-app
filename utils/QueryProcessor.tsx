05e5617f7af04c374cfc2cdce2be95e92abb4b33
elzlee <elizabethlee245@gmail.com>
2023-03-14 16:18:20 -0400

    Ergerg

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -1,7 +1,7 @@
 export default function QueryProcessor(query: string): string {
-  if (query.toLowerCase().includes("What is 36 plus 95?")) {
+  if (query.toLowerCase().includes("What is 21 plus 78?")) {
     return (
-      "131"
+      "99"
     );
   }
 
